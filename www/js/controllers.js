8073b890a216a224f530b97bba9a7454a6308d2b
Henry Millison <hmillison@gmail.com>
2015-01-17 15:54:56 -0500

    params work in ionic form, fixing up links to book flights

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,19 @@
 angular.module('starter.controllers', ['ui.router'])
 
-.controller('FormCtrl', function($scope, $http, $state) {
+.controller('FormCtrl', function($scope, $http, $state,$ionicViewSwitcher, $filter) {
   $scope.airport = 'DTW';
   $scope.price = 300;
-  // $scope.startDate = '2015-02-01';
-  // $scope.endDate = "2015-02-10";
+  $scope.startDate = 'Mar 01, 2015';
+  $scope.endDate = 'Mar 10, 2015';
   $scope.submit = function(){
     $scope.results = [];
-    $http.get('http://tripmeflights.herokuapp.com/search').then(function (resp){
+    this.startDate = $filter('date')(this.startDate,'yyyy-MM-dd');
+    this.endDate = $filter('date')(this.startDate,'yyyy-MM-dd');
+    $scope.url = 'http://tripmeflights.herokuapp.com/search?airport=' + this.airport + '&price=' + this.price + '&startDate=' + this.startDate + '&endDate=' + this.endDate;
+    $http.get($scope.url)
+    .then(function (resp){
       console.log('Success', resp);
+      $ionicViewSwitcher.nextDirection('forward');
       $state.go('tab.search', {results : JSON.stringify(resp.data)});
     }, function(err){
       console.log('ERR', err);
@@ -16,8 +21,15 @@ angular.module('starter.controllers', ['ui.router'])
   }
 })
 
-.controller('SearchCtrl', function($scope,$state,$stateParams) {
+.controller('SearchCtrl', function($scope,$state,$stateParams,$ionicViewSwitcher) {
   $scope.results = JSON.parse($stateParams.results);
   //$scope.results = ["1","22",", "]
-})
+  $scope.goBack = function() {
+    $ionicViewSwitcher.nextDirection('back');
+    $state.go('tab.form');
+  }
 
+  $scope.openLink = function(url){
+    window.open(url, '_blank', 'location=yes');
+  }
+})
